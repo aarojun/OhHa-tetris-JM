52c9391458321e432915d162e86e52f2d099113d
Aaro Juntunen <zaarock@zaa-Aspire-V3-772G.Elisa>
2014-01-16 18:56:46 +0200

    netbeans-projekti ja alustava dokumentointi lisätty

---
+++ tetris-JM/tetris-JM/src/main/java/tetris/tetrisjm/App.java
@@ -0,0 +1,13 @@
+package tetris.tetrisjm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
